7024d11f878924f53c4450bdd883ab5f6af83895
untalsebastianb <juan.bueno01@usc.edu.co>
2020-12-10 12:25:16 -0500

    Create function to render TDs to table

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
 const app = () => {
-  const ponerFilas = () => [
-    <tr>
-      <td>Sebastian</td>
-      <td>cgmail</td>
-      <td>sebastian.com</td>
-    </tr>,
-    <tr>
-      <td>camilo</td>
-      <td>cgmail</td>
-      <td>camilo.com</td>
-    </tr>,
+  const usersData = [
+    { name: "Camilo", email: "j.galvez@site.com", site: "camilo.tech.com" },
+    {
+      name: "Sebastian",
+      email: "juan.bueno@usc.co",
+      site: "sebastian.tech.com",
+    },
+    { name: "Lorena", email: "lorena@yaho.es", site: "lorena.da.com" },
   ];
+
+  const ponerFilas = () => {
+    return usersData.map((item, index) => (
+        <tr key={item.name + item.email}>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.site}</td>
+        </tr>
+    ));
+  };
 
   return (
     <div className="margen">
@@ -24,9 +31,7 @@ const app = () => {
             <th>Enlace</th>
           </tr>
         </thead>
-        <tbody>
-          {ponerFilas()}
-        </tbody>
+        <tbody>{ponerFilas()}</tbody>
       </table>
     </div>
   );
